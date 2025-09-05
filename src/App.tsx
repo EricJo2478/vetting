073c484edb05adf0da2dec2a24bc2ce83ba2976f
Eric <ericjo2478@gmail.com>
2025-09-05 08:27:21 -0600

    imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  signInWithPopup,
+  type User,
+} from "firebase/auth";
+import { useEffect, useState } from "react";
+import NavBar from "./components/Navbar";
+import { Accordion } from "react-bootstrap";
+import ModalButton from "./components/ModalButton";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
 
   return (
