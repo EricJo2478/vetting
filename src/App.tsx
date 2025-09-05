7352b9941f8463f3e64f1d6566bed4a8e94a3c58
Eric <ericjo2478@gmail.com>
2025-09-05 08:27:38 -0600

    firebase for auth and firestore

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,48 @@ import ModalButton from "./components/ModalButton";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBZo1hbq7C20pS_OK-ahyiUE96lcjAgWmQ",
+  authDomain: "vetting-caf2f.firebaseapp.com",
+  projectId: "vetting-caf2f",
+  storageBucket: "vetting-caf2f.firebasestorage.app",
+  messagingSenderId: "727627155798",
+  appId: "1:727627155798:web:e89bdc979c27e611c12ef7",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
+
+export default function App() {
+  const [page, setPage] = useState("home");
+  const [user, setUser] = useState(null as User | null);
+  const [loading, setLoading] = useState(true);
+  const [progress, setProgress] = useState({});
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setUser(user);
+      setLoading(false);
+    });
+
+    // Clean up the listener when the component unmounts
+    return () => unsubscribe();
+  }, []); // Empty dependency array ensures this runs once on mount
+
+  const handleGoogleLogin = async () => {
+    console.log(auth, googleProvider);
+    try {
+      const user = await signInWithPopup(auth, googleProvider);
+      setUser(user.user);
+      // User successfully logged in, redirect or update UI
+      console.log("User logged in with Google!");
+    } catch (error) {
+      console.error("Error during Google login:", error);
+    }
+  };
 
   return (
     <>
